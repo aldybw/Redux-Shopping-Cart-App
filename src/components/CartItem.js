59b41816085266f63a221c3f070092ec345aa2f2
aldyckybw <bagusaldycky@gmail.com>
2023-03-21 16:04:20 +0700

    first commit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { cartActions } from "../store/cart-slice";
 import "./Cart.css";
-import { cartActions } from "./../store/cartSlice";
 const CartItem = ({ name, quantity, total, price, id }) => {
   const dispatch = useDispatch();
-  const removeHandler = () => {
-    dispatch(cartActions.removeFromCart(id));
-  };
-  const addHandler = () => {
+  const incrementCartItem = () => {
     dispatch(
       cartActions.addToCart({
         id,
@@ -16,16 +13,19 @@ const CartItem = ({ name, quantity, total, price, id }) => {
       })
     );
   };
+  const decrementCartItems = () => {
+    dispatch(cartActions.removeFromCart(id));
+  };
   return (
     <div className="cartItem">
       <h2> {name}</h2>
       <p>${price} /-</p>
       <p>x{quantity}</p>
       <article>Total ${total}</article>
-      <button className="cart-actions" onClick={removeHandler}>
+      <button className="cart-actions" onClick={decrementCartItems}>
         -
       </button>
-      <button className="cart-actions" onClick={addHandler}>
+      <button className="cart-actions" onClick={incrementCartItem}>
         +
       </button>
     </div>
